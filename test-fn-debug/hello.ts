4ca5800c627ba38e6818112dac416f1ebd3f26d5
minid33 <jaimie@kalvex.dev>
2026-07-21 16:23:07 +0000

    chore: remove debug test artifacts

--- test-fn-debug/hello.ts
+++
@@ -1,2 +0,0 @@
-export const config = { auth: false }
-export default async function handler(req, ctx) { return { message: "hi", requestId: ctx.requestId } }
--- test-fn-debug/protected.ts
+++
@@ -1,2 +0,0 @@
-export const config = { auth: true }
-export default async function handler(req, ctx) { return { user: ctx.auth } }
